4e1361bc5b7e93c707e5d639557c470fbb86236b
Ghulam Jilani <ghulamjilani@ghulamjilani-HP-Compaq-6710b-GU727UP-ABA.(none)>
2012-11-12 20:17:32 +0500

    migrations fixed

--- db/migrate/20121018120545_create_votes.rb
+++ db/migrate/20121018120545_create_votes.rb
@@ -1,7 +1,6 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.integer  :count
       t.references :votable, :polymorphic => true
       t.timestamps
     end
