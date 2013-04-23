96085c76d85540614f2c895b78e8a376a9ee0d2b
Usman Asif <pyroxicated@gmail.com>
2013-04-23 16:36:38 +0500

    Merge branch 'master' of github.com:usmanasif/Coding-Queue into umar
    Conflicts:
    	.idea/workspace.xml
    	Gemfile
    	db/migrate/20121019072637_remove_column_count_votes.rb
    	db/schema.rb
    	log/searchd.development.pid

---
+++ app/assets/javascripts/askquestions.js
@@ -0,0 +1,9 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+jQuery(function() {
+    return $('#askquestion_tag_names').tokenInput('/tags.json', {
+        theme: 'facebook',
+        prePopulate: $('#askquestion_tag_names').data('load')
+    });
+});
