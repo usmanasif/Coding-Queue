bf718859eb1c1c7e9798d351b3a372514ca0b83b
Usman Asif <pyroxicated@gmail.com>
2013-05-13 16:21:24 +0500

    user or email

---
+++ app/helpers/users_view_helper.rb
@@ -0,0 +1,2 @@
+module UsersViewHelper
+end
---
+++ test/functional/users_view_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersViewControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
